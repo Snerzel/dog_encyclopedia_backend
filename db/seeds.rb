71d72153f36f543d61d9b17e475eb571c63f1eed
Snerzel <snerzel@github.com>
2022-05-25 20:45:50 -0700

    added group model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sporting = Group.create(name: 'Sporting')
+hound = Group.create(name: 'Hound')
+working = Group.create(name: 'Working')
+terrier = Group.create(name: 'terrier')
+toy = Group.create(name: 'toy')
+non-sporting = Group.create(name: 'Non-Sporting')
+herding = Group.create(name: 'Herding')
